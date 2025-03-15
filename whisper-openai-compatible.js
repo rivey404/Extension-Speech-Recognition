223da5e6f5b52fabf3ab2fe895243a6705f60f3a
rivey404 <166439270+rivey404@users.noreply.github.com>
2025-03-15 12:23:29 +0800

    Update whisper-openai-compatible.js
    no default model

--- whisper-openai-compatible.js
+++ whisper-openai-compatible.js
@@ -31,7 +31,7 @@ class WhisperOpenAICompatibleSttProvider {
         // Used when provider settings are updated from UI
         this.settings.endpoint = $('#whisper_openai_compatible_endpoint').val();
         this.settings.apiKey = $('#whisper_openai_compatible_api_key').val();
-        this.settings.model = $('#whisper_openai_compatible_model').val() || this.defaultSettings.model;
+        this.settings.model = $('#whisper_openai_compatible_model').val();
         console.debug(DEBUG_PREFIX + 'Updated settings');
     }
 
